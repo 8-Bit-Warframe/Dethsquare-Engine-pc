37918936a57ca4ee352b6be35f6516983b2f96f5
Ezard <ezard.ben@gmail.com>
2015-09-25 14:35:42 +0100

    Added desktop support for PlayerPrefs

--- Utils.java
+++ Utils.java
@@ -15,8 +15,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.prefs.Preferences;
 
 import javax.imageio.ImageIO;
 
@@ -24,10 +26,15 @@ public class Utils {
 	public static final Platform PLATFORM = Platform.DESKTOP;
 	private static final ArrayList<BufferedImage> images = new ArrayList<>();
 	private static BufferedImage temp = null;
+	private static final Preferences prefs = Preferences.userRoot().node("com/ezardlabs/lostsector");
 
 	public enum Platform {
 		ANDROID,
 		DESKTOP
+	}
+
+	public static void init() {
+
 	}
 
 	public static int[] loadImage(String path) {
@@ -121,5 +128,37 @@ public class Utils {
 		if (playingAudio.containsKey(audioClip)) {
 			playingAudio.remove(audioClip).stop();
 		}
+	}
+
+	public static void setBoolean(String key, boolean value) {
+		prefs.putBoolean(key, value);
+	}
+
+	public static void setInt(String key, int value) {
+		prefs.putInt(key, value);
+	}
+
+	public static void setFloat(String key, float value) {
+		prefs.putFloat(key, value);
+	}
+
+	public static void setString(String key, String value) {
+		prefs.putByteArray(key, value.getBytes(Charset.forName("UTF-8")));
+	}
+
+	public static boolean getBoolean(String key, boolean defaultValue) {
+		return prefs.getBoolean(key, defaultValue);
+	}
+
+	public static int getInt(String key, int defaultValue) {
+		return prefs.getInt(key, defaultValue);
+	}
+
+	public static float getFloat(String key, float defaultValue) {
+		return prefs.getFloat(key, defaultValue);
+	}
+
+	public static String getString(String key, String defaultValue) {
+		return new String(prefs.getByteArray(key, defaultValue.getBytes(Charset.forName("UTF-8"))), Charset.forName("UTF-8"));
 	}
 }
